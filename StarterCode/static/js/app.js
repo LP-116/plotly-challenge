2e6e3a8d13ebb3cb0b5e7fafe9ec7315ff78c8d6
Leanne <leanne_porter@hotmail.com>
2021-08-19 15:53:48 +1000

    updating code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -14,6 +14,10 @@ d3.json("samples.json").then((item) => {
 
 });
 
+var filterDropdown = d3.select("#selDataset");
+
+filterDropdown.on("change", displaygraph);
+
 
 function displaygraph() {
 
@@ -34,9 +38,21 @@ function displaygraph() {
         var values = idMatch.sample_values;
         console.log(values);
 
+        var top10_otu = otu.slice(0,10).reverse();
+        console.log(top10_otu);
+
+        var reformat_top10_otu = []
+        top10_otu.forEach((label) => {
+            reformat_top10_otu.push("OTU " + label);
+        });
+
+        var top10_values = values.slice(0,10).reverse();
+        console.log(top10_values);
+
         var trace1 = {
-            x: otu,
-            y: values,
+            x: top10_values,
+            y: reformat_top10_otu,
+            text: top10_otu,
             type: "bar",
             orientation: 'h',
             name: "Test"
@@ -45,8 +61,9 @@ function displaygraph() {
         var data = [trace1];
 
         var layout = {
-            xaxis: { title: "ID" },
-            yaxis: { title: "Value" }
+            xaxis: { title: "Value" },
+            yaxis: { title: "ID"},
+            margin: { t: 30, l: 150 }
         };
 
         Plotly.newPlot("bar", data, layout);
